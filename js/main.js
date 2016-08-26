e8d9ea311dbe09721dd62f275633ab2ea72837f8
Mark <marktaglieri@hotmail.com>
2016-08-27 10:50:51 +1200

    Skiube - a wild red square appears.

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 
 function preload() {
 	 game.load.image('star', 'assets/star.png');
+	 game.load.image('redSquare', 'assets/redSquare.png');
 }
 
 function create() {
 	game.add.sprite(0, 0, 'star');
+	game.add.sprite(100, 100, 'redSquare');
 }
 
 
